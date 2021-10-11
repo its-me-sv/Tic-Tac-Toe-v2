129935f3c5f61527ef7a866c43097efac1b478cb
Suraj Vijay <surajcpp115@gmail.com>
2021-10-12 00:39:01 +0530

    switched from browserrouter to hashrouter

--- src/containers/app/app.container.jsx
+++ src/containers/app/app.container.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, BrowserRouter as Router, Redirect} from "react-router-dom";
+import {Switch, Route, HashRouter} from "react-router-dom";
 
 import Home from "../../Pages/Home/home.page";
 import Multiplayer from "../../Pages/Multiplayer/Multiplayer.page";
@@ -7,16 +7,13 @@ import Room from "../../Pages/Room/Room.page";
 class App extends React.Component {
     render() {
         return (
-            <Router basename="/Tic-Tac-Toe-v2">
+            <HashRouter basename="/Tic-Tac-Toe-v2">
                 <Switch>
                     <Route exact path="/" component={Home}/>
                     <Route exact path="/multiplayer/rooms/:roomId" component={Room} />
                     <Route exact path="/multiplayer" component={Multiplayer} />
-                    <Route>
-                        <Redirect to="/" />
-                    </Route>
                 </Switch>
-            </Router>
+            </HashRouter>
         );
     }
 }
